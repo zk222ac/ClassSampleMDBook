983e23ea212595e43cac8eef4a42d5a28b2bc8f4
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-04-09 13:05:00 +0200

    release

---
+++ ClassSampleMDBook/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace ClassSampleMDBook
+{
+
+    public class Book
+    {
+        public ObjectId _id { get; set; }
+        public string name { get; set; }
+        public string dob { get; set; }
+        public string[] loves { get; set; }
+        public int weight { get; set; }
+        public string gender { get; set; }
+        public int vampires { get; set; }
+    }
+
+}
--- ClassSampleMDBook/Program.cs
+++ ClassSampleMDBook/Program.cs
@@ -1,21 +1,94 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace ClassSampleMDBook
 {
     class Program
     {
+        private const string ConnectionString = "mongodb://localhost:27017";
+        private const string DatabaseName = "DbBook";
+        public static IMongoCollection<BsonDocument> Collections { get; set; }
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Collections = MongoDbCollection();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Task no 1 : Inserting new document 
+            // InsertNewDocumentInBook(); //--> Enable or Disable 
+
+            // Task no 2 : Inserting multiple document 
+             //InsertMultipleDocumentInBook(); //--> Enable or Disable 
+
+            // Task no 3 : count documents
+             //var count = Collections.Count(new BsonDocument());
+             //Console.WriteLine($"Total Documents is :" + count);
+
+            // Task no 4 : Return only first Document
+            //ReturnFirstDocumentOnly();
+
+            // Task no 5 : Return only first Document
+            IterativelyProcessDocument();
+
+            Console.ReadKey();
         }
+      
+
+
+        private static IMongoCollection<BsonDocument> MongoDbCollection()
+        {
+            // client establish a  connection with server via connection string
+            var client = new MongoClient(ConnectionString);
+
+            // Define a server database 
+            var database = client.GetDatabase(DatabaseName);
+
+            // Get collection --> Get data collection Book from Mongo db
+            var collection = database.GetCollection<BsonDocument>("Book");
+
+            return collection;
+        }
+
+        public static async void InsertNewDocumentInBook()
+        {
+            var doc = new BsonDocument()
+           {
+               { "name", "MongoDB"},
+               { "type", "Database"},
+               {"count" , 1 },
+               {"info", new BsonDocument{{"x",203}, { "y",102}}},
+               { "Task" , "Mandatory Assignment"}
+           };
+
+            await Collections.InsertOneAsync(doc);
+        }
+
+        public static async void InsertMultipleDocumentInBook()
+        {
+            // generate 100 documents with a counter ranging from 0 - 99
+            var doc = Enumerable.Range(0, 5).Select(i => new BsonDocument
+            {
+                { "name1", "name1"},
+                { "type", "type"},
+                {"counter" , 1 },
+                {"info", new BsonDocument{{"x",203}, { "y",102}}}
+            });
+
+            await Collections.InsertManyAsync(doc);
+        }
+
+
+        public static async void ReturnFirstDocumentOnly()
+        {
+            var doc = await Collections.Find(new BsonDocument()).FirstOrDefaultAsync();
+            Console.WriteLine(doc);
+        }
+        public static async void IterativelyProcessDocument()
+        {
+           await Collections.Find(new BsonDocument()).ForEachAsync(d => Console.WriteLine(d));
+        }
+
+
     }
 }
